c05ffc9affeda219ed5f2f41c334c5b6bafba816
kushan <kushan.k1001@gmail.com>
2024-03-11 18:36:50 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './login.html','./signup.html','class_section_selection.html'
+    './login.html','./signup.html','./class_section_selection.html','./dashboard.html'
   ],
   theme: {
     extend: {},
